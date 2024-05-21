7b859f6392accb4b420072cd14f77476dfab4938
Rajeev Yadav <143483622+RajeevYadav1999@users.noreply.github.com>
2024-05-21 22:09:29 +0530

    added sign-in route and its working code

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js"
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken"
 
 export const signup = async (req, res, next)=>{
 
@@ -24,5 +25,30 @@ try{
     res.json({"message": "user created"})
 }catch(error){
     res.status(400).json({"message": error.message})
+}
+}
+
+export const signin = async (req, res, next) => {
+const {email, password} = req.body;
+if(!email || !password || email ==="" || password ===""){
+    next(errorHandler(400, "Please fill all the fields"))
+}
+try {
+    const validUser = await User.findOne({email})
+    if(!validUser){
+        return next(errorHandler(404, "User not found"))
+    }
+
+    const token = jwt.sign({id : validUser._id}, process.env.JWT_SECRET)
+
+    const {password : pass, ...rest} = validUser._doc
+    return res.status(200).cookie("access_token", token, {
+        httpOnly : true
+    })
+    .json(rest)
+
+} catch(error) {
+    console.log(error);
+    res.status(400).json({message : error.message})
 }
 }
