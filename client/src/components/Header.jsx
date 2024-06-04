652a84bb04409831b4f9fa7004c5668bfa6f19f4
Rajeev Yadav <143483622+RajeevYadav1999@users.noreply.github.com>
2024-06-04 18:25:20 +0530

    fixed header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -41,31 +41,33 @@ const Header = () => {
             </Button>
 
             {currentUser ? (
-              <Dropdown>
-                arrowIcon={false}
-                inline
-                label={
-                  <Avatar
-                  img={currentUser.profilePicture} 
-                  rounded
-                  />
-                }
-                <Dropdown.Header>
-                  <span className="block text-sm">@{currentUser.username}</span>
-                  <span className="block text-sm font-medium truncate">{currentUser.email}</span>
-                </Dropdown.Header>
-                <Link to ={'/dashboard?tab=profile'}>
-                  <Dropdown.Item>Profile</Dropdown.Item>
-                </Link>
-                <Dropdown.Divider>
-                <Dropdown.Item>Sign Out</Dropdown.Item>
-                </Dropdown.Divider>
-              </Dropdown>
-            ) : (
-        <Link to ="/Sign-in">
-        <Button outline gradientDuoTone="greenToBlue">Sign In</Button>
-        </Link>
-            )}
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser.profilePicture} rounded />
+            }
+          >
+                
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='purpleToBlue' outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
 
       <Navbar.Toggle/>
         </div>
